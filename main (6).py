a4365139533dda5ef9758200a91be326fd6b4b9e
abinishasibiwilson <145101232+abinishasibiwilson@users.noreply.github.com>
2023-10-06 10:45:51 +0530

    main (6).py

---
+++ main (6).py
@@ -0,0 +1,18 @@
+#define the base class player
+class player:
+  def play(self):
+   print("The player is playing cricket.")
+
+class batsman(player):
+  def play(self):
+    print("The batsman is batting.")
+
+class bowler(player):
+  def play(self):
+    print("The bowler is bowling.")
+
+batsman = batsman()
+bowler = bowler()
+
+batsman.play()
+bowler.play()
